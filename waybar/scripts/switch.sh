1e94f42cdfcc0df6d4736f48a99b09b0f2e9f9d0
MrSpaar <julien.pistre@icloud.com>
2024-09-02 17:42:59 +0200

    Fixed dunst theme swicthing

--- waybar/scripts/switch.sh
+++ waybar/scripts/switch.sh
@@ -20,8 +20,8 @@ hyprctl hyprpaper wallpaper ", $(OTHER "$WALLPAPER_PATH")"  > /dev/null
 sed -i "s|$(CURRENT "$ROFI_THEME")|$(OTHER "$ROFI_THEME")|" ~/.config/rofi/config.rasi
 sed -i "s|$(CURRENT "$QT_THEME")|$(OTHER "$QT_THEME")|" ~/.config/qt6ct/qt6ct.conf
 
-sed -i "s|background = \"$(CURRENT "$DUNST_BG_COLOR")\"|background = \"$(OTHER "$DUNST_BG_COLOR")\"|" ~/.config/dunst/dunstrc
-sed -i "s|foreground = \"$(CURRENT "$DUNST_FG_COLOR")\"|foreground = \"$(OTHER "$DUNST_FG_COLOR")\"|" ~/.config/dunst/dunstrc
+sed -i "s|background = \"#$(CURRENT "$DUNST_BG_COLOR")\"|background = \"#$(OTHER "$DUNST_BG_COLOR")\"|" ~/.config/dunst/dunstrc
+sed -i "s|foreground = \"#$(CURRENT "$DUNST_FG_COLOR")\"|foreground = \"#$(OTHER "$DUNST_FG_COLOR")\"|" ~/.config/dunst/dunstrc
 killall dunst > /dev/null 2>&1
 
 pkill -SIGRTMIN+8 waybar
